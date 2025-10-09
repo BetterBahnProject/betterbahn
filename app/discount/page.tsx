698f6534d73f5030549a2bfd7ed7bfc8c21b171d
FunctionDJ <34871211+FunctionDJ@users.noreply.github.com>
2025-10-09 20:56:42 +0200

    fix: fix next build SSR useSearchParams error

--- app/discount/page.tsx
+++ app/discount/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { Suspense } from "react";
 import { DiscountContent } from "./DiscountContent";
 
 const Discount = () => (
 	<section className="mt-16 w-full max-w-7xl mx-auto ">
-		<DiscountContent />
+		{/* Suspense is for useSearchParams https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout */}
+		<Suspense>
+			<DiscountContent />
+		</Suspense>
 	</section>
 );
 
